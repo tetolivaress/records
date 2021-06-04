6df6dcd7189016c4f9cc600aff5685efeb60900b
tetolivares <tetolivares@gmail.com>
2021-06-04 17:49:51 -0400

    working navigators

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/components/navigator/StackNavigator';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
  const App = () => {
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 
-const HomeScreen = () => (
-  <Text>Esta es la pantalla de Home</Text>
-)
+const HomeScreen = () => {
+  const navigator = useNavigation();
+  return (
+    <View style={{flex: 1}}>
+      <Text>Esta es la pantalla de Home</Text>
+      <TouchableOpacity
+        style={ styles.btn }
+        onPress={()=>navigator.navigate('HashTagsScreen')}
+      >
+        <Text style={{textAlign: 'center'}}>Go to hashtags</Text>
+      </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: 'red',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+    textAlign: 'center'
+  }
+})
 
 export default HomeScreen;
