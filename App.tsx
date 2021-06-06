2bbe41dcfcf83f97378a18575683aabbdc941b54
tetolivares <tetolivares@gmail.com>
2021-06-06 13:26:37 -0400

    fix to multiples navidators

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+import { DrawerNavigator } from './src/navigator/DrawerNavigator';
 
  const App = () => {
 
    return (
      <NavigationContainer>
-       <StackNavigator />
+       <DrawerNavigator />
      </NavigationContainer>
    );
  };
--- src/navigator/DrawerNavigator.tsx
+++ src/navigator/DrawerNavigator.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useWindowDimensions } from 'react-native';
+import { StackNavigator } from './StackNavigator';
 import HashTagsScreen from '../screens/HashTagsScreen';
+import RecordsScreen from '../screens/RecordsScreen';
 
 const Drawer = createDrawerNavigator();
 
-export const DrawerNavigation = () => {
+export const DrawerNavigator = () => {
   const { width } = useWindowDimensions();
 
   return (
     <Drawer.Navigator
       drawerType={ width >= 768 ? 'permanent' : 'front' }
     >
-      <Drawer.Screen name="Hashtags" component={HashTagsScreen} />
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="HashTagsScreen" component={HashTagsScreen} />
+      <Drawer.Screen name="RecordsScreen" component={RecordsScreen} />
     </Drawer.Navigator>
   )
 }
