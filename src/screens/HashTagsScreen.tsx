cef01b48d501107b2df8da17d052b97ec6ba1cf5
tetolivares <tetolivares@gmail.com>
2021-06-07 17:57:03 -0400

    movies interface created

--- src/screens/HashTagsScreen.tsx
+++ src/screens/HashTagsScreen.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Text } from "react-native";
-import { API_TOKEN } from "@env"
+import { useEffect, useState } from 'react';
+import movieDB from "../api/movieDB";
 
 
-const HashTagsScreen = () => (
-  <Text>{ API_TOKEN }</Text>
-)
+const HashTagsScreen = () => {
+  
+  const [state, setstate] = useState('just')
+  useEffect(()=>{
+
+    movieDB.get('/now_playing')
+      .then( res => {
+        console.log(JSON.stringify(res.data, null, 3));
+        setstate('loaded')
+      });
+
+  },[]);
+  
+  return (
+    <Text>{state}</Text>
+  );
+}
 
 export default HashTagsScreen;
