92baf8deb646a31bd6bddace94bfe228e4ab4b95
tetolivares <tetolivares@gmail.com>
2021-06-09 16:30:19 -0400

    to test in house

--- src/screens/HashTagsScreen.tsx
+++ src/screens/HashTagsScreen.tsx
@@ -1,24 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
+import { ActivityIndicator, Image, Text, View } from "react-native";
 import { useEffect, useState } from 'react';
 import movieDB from "../api/movieDB";
+import { Movie, MovieDBNowPlaying } from '../interfaces/movie';
+import { ScrollView } from "react-native-gesture-handler";
 
 
 const HashTagsScreen = () => {
   
-  const [state, setstate] = useState('just')
+  const [state, setstate] = useState<Movie[]>([])
   useEffect(()=>{
 
-    movieDB.get('/now_playing')
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then( res => {
         console.log(JSON.stringify(res.data, null, 3));
-        setstate('loaded')
+        setstate(res.data.results)
       });
 
   },[]);
   
   return (
-    <Text>{state}</Text>
+    <>
+      <ScrollView>
+        {
+          state
+            ? state.map((item: Movie) => (
+              <View style={{elevation: 5, marginVertical: 25, backgroundColor: 'azure'}} key={item.id.toString()}>
+                <Text key={item.id.toString()} style={{fontSize: 18, padding: 12}}>{item.title}</Text>
+                <Image
+                  source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}
+                  style={{width: 100, height: 100, alignSelf: 'flex-end', margin: 20}}
+                />
+              </View>
+              ))
+            : <ActivityIndicator size={80} color='#e3e3e3' />
+        }
+      </ScrollView>
+    </>
   );
 }
 
